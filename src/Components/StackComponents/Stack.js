f5c6bb2a72d24ab6294f23c5580b24482fd05a86
k-westbrook <kcheriewestbrook@gmail.com>
2019-08-28 22:48:26 -0700

    updated colors

--- src/Components/StackComponents/Stack.js
+++ src/Components/StackComponents/Stack.js
@@ -54,7 +54,7 @@ class Stack extends React.Component {
           <h3 className='ds-header'>Stack</h3>
           <div className='ds-explanation'>
             <p><b>What is a stack?</b> A stack is a data strucutre that stores data like it says in its name --like a stack of pancakes.
-          It follows the LIFO- Last In, First Out Order. You push an element in and another and another. The elements are removed by the pop function which pops from the end.  The peek function looks at the element next to be popped. Try a stack out!</p>
+            It follows the LIFO- Last In, First Out Order. You push an element in and another and another. The elements are removed by the pop function which pops from the end.  The peek function looks at the element next to be popped. Try a stack out!</p>
           </div>
         </div>
         {
