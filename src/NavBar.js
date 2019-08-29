8cd9c6a9b5d59d80456786b604a09b4f7a753408
k-westbrook <kcheriewestbrook@gmail.com>
2019-08-28 23:40:18 -0700

    updated mobile

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,17 +9,17 @@ class Routes extends React.Component {
   render() {
     return (
       <div className='nav-bar-container'>
-        <div >
+        <div className='nav-bar-item' >
           <Link className='link' to="/">
             <h3>Home</h3>
           </Link>
         </div>
-        <div>
+        <div className='nav-bar-item'>
           <Link className='link' to="/stack">
             <h3>Stack</h3>
           </Link>
         </div>
-        <div>
+        <div className='nav-bar-item'>
           <Link className='link' to="/binarysearchtree">
             <h3>Binary Search Tree</h3>
           </Link>
